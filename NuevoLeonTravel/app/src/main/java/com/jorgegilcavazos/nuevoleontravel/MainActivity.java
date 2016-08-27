52139c2f67886528be57d5780258f5f5a9bd1139
Diego Iván Contreras Villegas <Diego Ivn Contreras Villegas>
2016-08-27 17:21:56 -0500

    Modify Activity, add icons to menu

--- NuevoLeonTravel/app/src/main/java/com/jorgegilcavazos/nuevoleontravel/MainActivity.java
+++ NuevoLeonTravel/app/src/main/java/com/jorgegilcavazos/nuevoleontravel/MainActivity.java
@@ -91,17 +91,19 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_inicio) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_quehacer) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_transporte) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_hoteles) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_eventos) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_cuenta) {
+
+        }else if (id == R.id.nav_ajustes) {
 
         }
 
